407466456c238377b7eb295323cb61da4ac82dd7
Vyacheslav Ponomarev <49794467+sohho888@users.noreply.github.com>
2022-03-18 11:00:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function descendingOrder(n) {
-    let arr2 = []
-    let arr = String(n).split("");
-    for (let i = 0; i < arr.length; i++) {
-        if (Number(arr[i]) >= Number(arr[i+1]))
-            arr2.push(Number(arr[i]))
-        // else {
-        //     arr2.push(Number(arr[i]))
-        // }
-    }
-    return arr2;
-}
+const descendingOrder = (n, arr = [...String(n)]) => parseInt(arr.sort((a, b) => b - a).join(''));
 
-
-console.log(descendingOrder(588824543)); 
+let fff = descendingOrder(56489764895871336);
+console.log(fff);
