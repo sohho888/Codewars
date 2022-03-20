ed81e8412349d7a4545c16f52d21b696f188b385
Vyacheslav Ponomarev <49794467+sohho888@users.noreply.github.com>
2022-03-20 14:20:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const descendingOrder = (n, arr = [...String(n)]) => parseInt(arr.sort((a, b) =>
 
 let fff = descendingOrder(56489764895871336);
 console.log(fff);
+
+
+
+// Powers of 2.1
+
+function powersOfTwo(n){
+    let arr = []
+    for(let i = 0; i <= n; i++) {
+      let f = Math.pow(2,i);
+      arr.push(f)
+    }
+   
+    
+    return arr
+  }
+  
+  let jj = powersOfTwo(4);
+  console.log(jj);
